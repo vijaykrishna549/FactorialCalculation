3c124e488ff9a50fbfc718244366d48cdd67f037
pvijaykrishna549 <you@example.com>
2021-04-22 15:01:50 +0530

    Factorial_Calculation_WithoutRecursion

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, factorial = 1, number;
+            Console.Write("Enter a Number: ");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                factorial = factorial * i;
+            }
+            Console.Write("Factorial of the Number is: " + factorial);
+        
+        }
+
+    }
+}
